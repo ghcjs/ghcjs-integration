977d629ffb912107e1d62d9130840ab5702ce7fd
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-01-08 20:37:30 +0000

    Add files

---
+++ nix/hix.nix
@@ -0,0 +1,14 @@
+{config, pkgs, ...}: {
+  compiler-nix-name = "ghc981";
+  modules = [({pkgs, ...}: {
+    packages.webkit2gtk3-javascriptcore.components.library.doHaddock = false;
+  })];
+  shell.buildInputs = [ pkgs.nodejs ];
+  shell.tools.cabal = {};
+  shell.crossPlatforms = p: pkgs.lib.optional (__elem config.compiler-nix-name ["ghc8107" "ghc981"]) p.ghcjs;
+
+  # Use this for checking if `aeson` 2 works (tests will not build because `webdriver` still needs aeson <2)
+  cabalProjectLocal = ''
+    constraints: aeson >=2
+  '';
+}
