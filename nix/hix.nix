354bdad335d6d8f774e608966659e4421697398a
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-02-07 23:16:39 +1300

    Update

--- nix/hix.nix
+++ nix/hix.nix
@@ -3,6 +3,7 @@
   modules = [({pkgs, ...}: {
     packages.webkit2gtk3-javascriptcore.components.library.doHaddock = false;
   })];
+  flake.variants.ghc8107.compiler-nix-name = pkgs.lib.mkForce "ghc8107";
   shell.buildInputs = [ pkgs.nodejs ];
   shell.tools.cabal = {};
   crossPlatforms = p: pkgs.lib.optional (__elem config.compiler-nix-name ["ghc8107" "ghc981"]) p.ghcjs;
