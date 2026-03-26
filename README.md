a863a303aa7071c21641981fbd46e3d333536b8d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2026-03-26 17:38:53 +1300

    Update to haskell.nix latest, GHC 9.14 default, bump submodule versions
    - Update haskell.nix flake input to latest
    - Default compiler: ghc914 (GHC 9.14.1)
    - Add ghc912 variant
    - Relax dependency bounds for GHC 9.14 (base 4.22, containers 0.8, template-haskell 2.24, time 1.15)
    - Use source constraints for exceptions, filepath, os-string, file-io, time, hashable, unordered-containers, directory
    - Fix hoogle solver to pick hoogle >= 5
    - Bump versions: jsaddle 0.9.9.4, jsaddle-clib 0.9.9.4, jsaddle-warp 0.9.9.6, jsaddle-dom 0.9.9.3, ghcjs-dom 0.9.9.3, ghcjs-dom-jsaddle 0.9.9.1

---
+++ README.md
@@ -0,0 +1,7 @@
+This repository contains submodules and project files for working on various GHCJS libraries before uploading them to hackage.org.
+
+To start a `nix develop` shell use:
+
+```
+nix develop '.?submodules=1#'
+```
--- nix/hix.nix
+++ nix/hix.nix
@@ -1,13 +1,18 @@
 {config, pkgs, ...}: {
-  compiler-nix-name = "ghc912";
+  compiler-nix-name = "ghc914";
   modules = [({pkgs, ...}: {
     package-keys = ["webkit2gtk3-javascriptcore"];
     packages.webkit2gtk3-javascriptcore.components.library.doHaddock = false;
     enableStatic = !pkgs.stdenv.hostPlatform.isGhcjs;
   })];
+  flake.variants.ghc912.compiler-nix-name = pkgs.lib.mkForce "ghc912";
   flake.variants.ghc967.compiler-nix-name = pkgs.lib.mkForce "ghc967";
   shell.buildInputs = [ pkgs.pkgsBuildBuild.nodejs ];
   shell.tools.cabal = {};
+  shell.tools.hoogle.cabalProjectLocal = ''
+    constraints: hoogle >= 5
+    allow-newer: *:base, *:containers, *:template-haskell
+  '';
   crossPlatforms = p: [ p.ghcjs ];
 
   # Use this for checking if `aeson` 2 works (tests will not build because `webdriver` still needs aeson <2)
